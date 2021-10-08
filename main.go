fe0116853eb86925ff7d56e5c2f526b2a7087791
Shelly Mohanty <shellymohanty09@gmail.com>
2021-10-08 22:06:57 +0530

    Connection established and two endpoints created

--- main.go
+++ main.go
@@ -1,21 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"github.com/gorilla/mux"
+)
 
 type User struct {
-	ID       string `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name     string `json:"name,omitempty" bson:"name,omitempty"`
-	Email    string `json:"email,omitempty" bson:"email,omitempty"`
-	Password string `json:"password,omitempty" bson:"password,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
 }
 
 type Posts struct {
-	ID              string `json:"_id,omitempty" bson:"_id,omitempty"`
-	Caption         string `json:"caption,omitempty" bson:"caption,omitempty"`
-	ImageURL        string `json:"imageUrl,omitempty" bson:"imageUrl,omitempty"`
-	PostedTimestamp string `json:"postedTimestamp,omitempty" bson:"postedTimestamp,omitempty"`
+	ID              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Caption         string             `json:"caption,omitempty" bson:"caption,omitempty"`
+	ImageURL        string             `json:"imageUrl,omitempty" bson:"imageUrl,omitempty"`
+	PostedTimestamp string             `json:"postedTimestamp,omitempty" bson:"postedTimestamp,omitempty"`
+}
+
+var client *mongo.Client
+
+func ConnectMongo() {
+	var mongoURL = "mongodb://localhost:27017"
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURL))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		fmt.Println("could not ping to mongo db service: %v\n", err)
+		return
+	}
+	fmt.Println("connected to nosql database:", mongoURL)
+}
+
+func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("InstagramBackendAPI").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var user User
+	collection := client.Database("InstagramBackendAPI").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(user)
 }
 
 func main() {
 	fmt.Println("Hello World")
+	ConnectMongo()
 }
